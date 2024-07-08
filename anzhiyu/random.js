fec426e45a2b88491f16f9f65e6ca598ad2ead60
imzyb <vanness.x@gmail.com>
2024-07-08 22:35:38 +0800

    Site updated: 2024-07-08 22:35:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/08/这是一篇新的博文/","2024/07/08/hello-world/"];function toRandomPost(){
+var posts=["undefined/这是一篇新的博文/","undefined/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
