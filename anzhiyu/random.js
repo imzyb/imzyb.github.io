a697a2f98f92142168293c809a531b84d9666646
imzyb <vanness.x@gmail.com>
2024-09-24 23:22:51 +0800

    Site updated: 2024-09-24 23:22:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/这是一篇新的博文/","undefined/hello-world/"];function toRandomPost(){
+var posts=["undefined/PVE8直通核显（N100），并开启虚拟化Sriov/","undefined/这是一篇新的博文/","undefined/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
