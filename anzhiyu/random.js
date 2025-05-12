a94f5d094a6d9d57b66f2f92ad57c9907b1177bd
imzyb <vanness.x@gmail.com>
2025-05-12 21:17:50 +0800

    Site updated: 2025-05-12 21:17:50

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["undefined/常用VPS脚本/","undefined/PVE8直通核显（N100），并开启虚拟化Sriov/","undefined/这是一篇新的博文/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
