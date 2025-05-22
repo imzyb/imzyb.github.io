0f64514bd00738d3ba65bbe41b611ccbdcfc59d1
imzyb <vanness.x@gmail.com>
2025-05-22 21:02:16 +0800

    Site updated: 2025-05-22 21:02:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/用Mihomo打造最强代理机/","undefined/常用VPS脚本/","undefined/PVE8直通核显（N100），并开启虚拟化Sriov/","undefined/这是一篇新的博文/"];function toRandomPost(){
+var posts=["undefined/用Mihomo打造最强代理机/","undefined/常用VPS脚本/","undefined/PVE8直通核显（N100），并开启虚拟化Sriov/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
